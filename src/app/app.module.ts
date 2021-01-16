9773386c070e926dfa284e68cbcda432d6adf7ea
Beeckaa <rebeckalindskog@gmail.com>
2021-01-16 18:31:30 +0100

    Add card component base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from './material-module';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CardComponent } from './../components/card/card.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/components/card/card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Card with multiple sections
+ */
+@Component({
+  selector: 'card-component',
+  templateUrl: 'card.component.html',
+  styleUrls: ['card.component.scss'],
+})
+export class CardComponent {}
